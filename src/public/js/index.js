684df296f4bdf23c527a568ad703c04398a48fda
Nicholas Surmava <nicholas.surmava@gmail.com>
2021-02-26 15:49:10 -0600

    added a fetch function to retrieve weather info from server

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,3 +1,32 @@
 (() => {
-	console.log('hello');
+  getWeather = () => {
+    try {
+      console.log("Getting weather!");
+
+      fetch("/getWeather")
+        .then((response) => response.json())
+        .then((weather) => {
+          console.log(weather);
+          const temp_el = document.querySelector(
+            '[data-js="weather__temperature"]'
+          );
+          const time_el = document.querySelector('[data-js="weather__time"]');
+          const lat_el = document.querySelector(
+            '[data-js="weather__latitude"]'
+          );
+          const long_el = document.querySelector(
+            '[data-js="weather__longitude"]'
+          );
+
+          temp_el.innerHTML = `TEMPERATURE ${weather.TEMPERATURE} C`;
+          time_el.innerHTML = `TIME ${weather.TIME_STRING}`;
+          lat_el.innerHTML = `LATITUDE ${weather.LATITUDE}`;
+          long_el.innerHTML = `LONGITUDE ${weather.LONGITUDE}`;
+        });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  getWeather();
 })();
